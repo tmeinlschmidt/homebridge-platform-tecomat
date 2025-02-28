a694ce94d5c82b287a634a7739950d2c2dec37d1
Tom Meinlschmidt <tomas@meinlschmidt.org>
2025-02-28 22:20:39 +0100

    wip

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'PlcJalousiePlatform';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plc-jalousie';
+export const PLUGIN_NAME = 'homebridge-platform-tecomat';
